5ce52918dfe11b0805abd01caf15e0e1085755c6
Alpha-gone <990131@naver.com>
2022-05-23 19:59:06 +0900

    22.05.23 - Response 중간까지

---
+++ response/src/main/java/com/example/response/dto/User.java
@@ -0,0 +1,19 @@
+package com.example.response.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class User {
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private String address;
+}
