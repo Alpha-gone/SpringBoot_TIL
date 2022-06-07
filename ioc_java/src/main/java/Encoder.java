11b1faceed0a3cbfa09c7ed6219cdd89effe6560
Alpha-gone <990131@naver.com>
2022-06-07 23:15:47 +0900

    22.06.07 - ioc 폴더명 변경 및 interceptor, 비동기 처리 완료

---
+++ ioc_java/src/main/java/Encoder.java
@@ -0,0 +1,13 @@
+public class Encoder {
+    private IEncoder iEncoder;
+
+    public Encoder(IEncoder iEncoder){
+        this.iEncoder = iEncoder;
+    }
+    public void setIEncoder(IEncoder iEncoder){
+        this.iEncoder = iEncoder;
+    }
+    public String encode(String message){
+        return iEncoder.encode(message);
+    }
+}
